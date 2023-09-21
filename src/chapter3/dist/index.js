62b508c1810a2dac250aad97f73cad9bb0c6f9a1
toona <amanou.toona@gmail.com>
2023-09-21 23:43:34 +0900

    chapter-3 section-011

--- src/chapter3/dist/index.js
+++ src/chapter3/dist/index.js
@@ -1,6 +1,30 @@
 "use strict";
-const sayHello = (name) => {
-    return `Hello ${name}`;
+// const sayHello: (name: string) => string = (name) => {
+//     return `Hello ${name}`;
+// }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-// console.log(sayHello('jack'))
-process.stdout.write(sayHello('jack'));
+// // console.log(sayHello('jack'))
+// process.stdout.write(sayHello('jack'))
+const printLine = (text, breakline = true) => {
+    process.stdout.write(text + (breakline ? '\n' : ''));
+};
+const promptInput = (text) => __awaiter(void 0, void 0, void 0, function* () {
+    printLine(`\n${text}\n> `, false);
+    const input = yield new Promise((resolve) => process.stdin.once('data', (data) => resolve(data.toString())));
+    return input.trim();
+});
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const name = yield promptInput('名前を入力してください');
+    console.log(name);
+    const age = yield promptInput('年令を入力してください');
+    console.log(age);
+    process.exit();
+}))();
--- src/chapter3/src/index.ts
+++ src/chapter3/src/index.ts
@@ -1,6 +1,37 @@
-const sayHello: (name: string) => string = (name) => {
-    return `Hello ${name}`;
-}
+const printLine = (text: string, breakline: boolean = true) => {
+    process.stdout.write(text + (breakline ? '\n' : ''))
+};
 
-// console.log(sayHello('jack'))
-process.stdout.write(sayHello('jack'))
+const promptInput = async (text: string) => {
+    printLine(`\n${text}\n> `, false)
+    const input: string = await new Promise((resolve) => process.stdin.once('data', (data) => resolve(data.toString())))
+    return input.trim()
+};
+
+
+(async () => {
+    const name = await promptInput('名前を入力してください');
+    console.log(name);
+    const age = await promptInput('年令を入力してください');
+    console.log(age);
+    process.exit();
+})()
+
+//  こちらでも動作する
+// const promptInput = (text: string) => {
+//     return new Promise((resolve, reject) => {
+//         printLine(`\n${text}\n >`, false);
+
+//         process.stdin.once('data', (data) => {
+//             const input = data.toString().trim();
+//             resolve(input);
+//         });
+
+//         process.stdin.once('error', (err) => {
+//             reject(err);
+//         });
+//     });
+// };
+
+// promptInput('名前を入力してください').then((input) => console.log(input)).catch((error) => { console.log(`error: ${error}`) })
+
