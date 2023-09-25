0a2583eb583bc5c0ee1fab3221401f418597bfea
toona <amanou.toona@gmail.com>
2023-09-26 00:20:48 +0900

    ~ p82

--- src/chapter3/dist/index.js
+++ src/chapter3/dist/index.js
@@ -1,7 +1,4 @@
 "use strict";
-// const sayHello: (name: string) => string = (name) => {
-//     return `Hello ${name}`;
-// }
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -11,8 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-// // console.log(sayHello('jack'))
-// process.stdout.write(sayHello('jack'))
+Object.defineProperty(exports, "__esModule", { value: true });
 const printLine = (text, breakline = true) => {
     process.stdout.write(text + (breakline ? '\n' : ''));
 };
@@ -21,10 +17,97 @@ const promptInput = (text) => __awaiter(void 0, void 0, void 0, function* () {
     const input = yield new Promise((resolve) => process.stdin.once('data', (data) => resolve(data.toString())));
     return input.trim();
 });
+//  こちらでも動作する
+// const promptInput = (text: string) => {
+//     return new Promise((resolve, reject) => {
+//         printLine(`\n${text}\n >`, false);
+//         process.stdin.once('data', (data) => {
+//             const input = data.toString().trim();
+//             resolve(input);
+//         });
+//         process.stdin.once('error', (err) => {
+//             reject(err);
+//         });
+//     });
+// };
+// promptInput('名前を入力してください').then((input) => console.log(input)).catch((error) => { console.log(`error: ${error}`) })
+class HitAndBlow {
+    constructor(mode) {
+        this.answerSource = Array.from({ length: 10 }, (_, index) => String(index));
+        this.answer = [];
+        this.tryCount = 0;
+        this.mode = mode;
+    }
+    ;
+    setting() {
+        const answerLength = this.getAnswerLength();
+        while (this.answer.length < answerLength) {
+            const randNum = Math.floor(Math.random() * answerLength);
+            const selectedItem = this.answerSource[randNum];
+            if (!this.answer.includes(selectedItem)) {
+                this.answer.push(selectedItem);
+            }
+        }
+    }
+    play() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const AnswerLenth = this.getAnswerLength();
+            const inputArr = (yield promptInput(`${AnswerLenth} 個の数字を入力してください`)).split(' ');
+            if (!this.validate(inputArr)) {
+                printLine('無効な入力です');
+                yield this.play();
+                return;
+            }
+            const result = this.check(inputArr);
+            if (result.hit === this.answer.length) {
+                this.tryCount += 1;
+                return;
+            }
+            printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`);
+            this.tryCount += 1;
+            yield this.play();
+        });
+    }
+    check(input) {
+        let hitCount = 0;
+        let blowCount = 0;
+        input.forEach((val, index) => {
+            if (val === this.answer[index]) {
+                hitCount += 1;
+            }
+            else {
+                blowCount += 1;
+            }
+        });
+        return { hit: hitCount, blow: blowCount };
+    }
+    end() {
+        printLine(`正解です\n試行回数: ${this.tryCount}`);
+        process.exit();
+    }
+    validate(inputArr) {
+        if (inputArr.length !== this.answer.length) {
+            return false;
+        }
+        if (!inputArr.every((val) => this.answerSource.includes(val))) {
+            return false;
+        }
+        if (inputArr.every((val, i) => inputArr.indexOf(val) !== i)) {
+            return false;
+        }
+        return true;
+    }
+    getAnswerLength() {
+        switch (this.mode) {
+            case 'normal': return 3;
+            case 'hard': return 4;
+        }
+    }
+}
+;
 (() => __awaiter(void 0, void 0, void 0, function* () {
-    const name = yield promptInput('名前を入力してください');
-    console.log(name);
-    const age = yield promptInput('年令を入力してください');
-    console.log(age);
-    process.exit();
+    const hitAndBlow = new HitAndBlow('normal');
+    hitAndBlow.setting();
+    yield hitAndBlow.play();
+    hitAndBlow.end();
 }))();
--- src/chapter3/src/index.ts
+++ src/chapter3/src/index.ts
@@ -1,3 +1,5 @@
+import { threadId } from "worker_threads";
+
 const printLine = (text: string, breakline: boolean = true) => {
     process.stdout.write(text + (breakline ? '\n' : ''))
 };
@@ -8,14 +10,6 @@ const promptInput = async (text: string) => {
     return input.trim()
 };
 
-
-(async () => {
-    const name = await promptInput('名前を入力してください');
-    console.log(name);
-    const age = await promptInput('年令を入力してください');
-    console.log(age);
-    process.exit();
-})()
 
 //  こちらでも動作する
 // const promptInput = (text: string) => {
@@ -35,3 +29,92 @@ const promptInput = async (text: string) => {
 
 // promptInput('名前を入力してください').then((input) => console.log(input)).catch((error) => { console.log(`error: ${error}`) })
 
+class HitAndBlow {
+    private readonly answerSource: string[];
+    private answer: string[];
+    private tryCount: number;
+    private mode: 'normal' | 'hard';
+
+    constructor(mode: 'normal' | 'hard') {
+        this.answerSource = Array.from({ length: 10 }, (_, index) => String(index));
+        this.answer = [];
+        this.tryCount = 0;
+        this.mode = mode;
+    };
+
+    setting() {
+        const answerLength = this.getAnswerLength();
+
+        while (this.answer.length < answerLength) {
+            const randNum = Math.floor(Math.random() * answerLength);
+            const selectedItem = this.answerSource[randNum];
+            if (!this.answer.includes(selectedItem)) {
+                this.answer.push(selectedItem);
+            }
+        }
+    }
+
+    async play() {
+        const AnswerLenth = this.getAnswerLength();
+        const inputArr = (await promptInput(`${AnswerLenth} 個の数字を入力してください`)).split(' ')
+
+        if (!this.validate(inputArr)) {
+            printLine('無効な入力です');
+            await this.play();
+            return
+        }
+
+        const result = this.check(inputArr);
+
+        if (result.hit === this.answer.length) {
+            this.tryCount += 1;
+            return;
+        }
+
+        printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`);
+        this.tryCount += 1;
+        await this.play();
+    }
+
+    private check(input: string[]) {
+        let hitCount = 0;
+        let blowCount = 0;
+
+        input.forEach((val, index) => {
+            if (val === this.answer[index]) {
+                hitCount += 1;
+            } else {
+                blowCount += 1;
+            }
+        })
+        return { hit: hitCount, blow: blowCount };
+    }
+
+    end() {
+        printLine(`正解です\n試行回数: ${this.tryCount}`);
+        process.exit()
+    }
+
+    private validate(inputArr: string[]) {
+        if (inputArr.length !== this.answer.length) { return false; }
+        if (!inputArr.every((val) => this.answerSource.includes(val))) { return false; }
+        if (inputArr.every((val, i) => inputArr.indexOf(val) !== i)) { return false; }
+        return true;
+    }
+
+    private getAnswerLength() {
+        switch (this.mode) {
+            case 'normal': return 3
+            case 'hard': return 4
+        }
+    }
+
+
+};
+
+(async () => {
+    const hitAndBlow = new HitAndBlow('normal');
+    hitAndBlow.setting();
+    await hitAndBlow.play();
+    hitAndBlow.end();
+})()
