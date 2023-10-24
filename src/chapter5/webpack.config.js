6abfca8be68a2d44d923c193d5d7641981a85a6e
toona <amanou.toona@gmail.com>
2023-10-25 00:25:01 +0900

    環境修正

--- src/chapter5/webpack.config.js
+++ src/chapter5/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   resolve: {
-    extensions: [".ts", ".js", "jsx"],
+    extensions: [".ts", ".js", ".jsx"],
   },
   module: {
     rules: [
